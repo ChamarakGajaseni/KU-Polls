253a108453d6019b073782034ee88f2130ad3c16
chamarak <chamarak.g@ku.th>
2024-09-10 10:54:25 +0700

    updated vote to once per question

--- polls/views.py
+++ polls/views.py
@@ -54,56 +54,35 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     user = request.user
 
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-        
-    except (KeyError, not question.can_vote):
+    # Ensure the question is open for voting
+    if not question.can_vote():
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "The Question is not pending currently.",
         })
-        
+
+    # Check if the user already voted for this question
+    user_vote = Vote.objects.filter(user=user, choice__question=question).first()
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
+        # Redisplay the question voting form if no choice was selected
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
-    
-    try:
-        vote = Vote.objects.get(user=user, choice=selected_choice)
 
-    except Vote.DoesNotExist:
+    if user_vote:
+        # If the user has already voted for this question, prevent additional voting
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You have already voted for this question.",
+        })
+    else:
+        # If no previous vote exists, create a new vote
         vote = Vote.objects.create(user=user, choice=selected_choice)
+        vote.save()
         messages.success(request, f"Your vote '{selected_choice.choice_text}' was recorded")
 
-    else:
-        vote.choice = selected_choice
-        messages.success(request, f"Your vote was changed to '{selected_choice.choice_text}'")
-    
-    vote.save()
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    
-    
-    # try:
-    #     selected_choice = question.choice_set.get(pk=request.POST['choice'])
-        
-    # except (KeyError, not question.can_vote):
-    #     return render(request, 'polls/detail.html', {
-    #         'question': question,
-    #         'error_message': "The Question is not pending currently.",
-    #     })
-        
-    # except (KeyError, Choice.DoesNotExist):
-    #     # Redisplay the question voting form.
-    #     return render(request, 'polls/detail.html', {
-    #         'question': question,
-    #         'error_message': "You didn't select a choice.",
-    #     })
-    # else:
-    #     selected_choice.votes += 1
-    #     selected_choice.save()
-    #     # Always return an HttpResponseRedirect after successfully dealing
-    #     # with POST data. This prevents data from being posted twice if a
-    #     # user hits the Back button.
-    #     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
